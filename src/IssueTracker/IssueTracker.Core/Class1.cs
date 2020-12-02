1bcc8bd9e0dc5af987f03ba0c082d84f732b3be8
mattiasback <mattias.j.back@gmail.com>
2020-12-02 19:33:33 +0100

    Init commit

---
+++ src/IssueTracker/IssueTracker.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IssueTracker.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/IssueTracker/IssueTracker.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IssueTracker.Data
+{
+    public class Class1
+    {
+    }
+}
