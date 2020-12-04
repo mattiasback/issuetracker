4f922509c785ef407577568fe5e3985ec2d18270
mattiasback <mattias.j.back@gmail.com>
2020-12-04 13:21:46 +0100

    Recreate solution as console app

---
+++ src/IssueTracker/IssueTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IssueTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Issue Tracker Engine");
+        }
+    }
+}
